0cf376f87bb371479dcd7280198ed74c913fab28
traximus <traximus.td@gmail.com>
2015-02-04 19:11:44 +0800

    add information
    add explanation for parameter paddingZero

--- MyAES/MyAES.h
+++ MyAES/MyAES.h
@@ -21,6 +21,7 @@
 对二进制数据加密 -  结果为已加密后的二进制数据的十六进制字符串
    paddingZero - 在进行字符串加密的时候，是否添加二进制数据0，
                  目的在于帮助c语言端判断是否到达字符串结尾；
+                 若服务器端没有进行此处理，可以传NO;
 */
 -(NSString *)aesEncryptData:(NSData *)dataT withSecret:(NSString *)secret needPaddingZero:(BOOL)paddingZero;
 
